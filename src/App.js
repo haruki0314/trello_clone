6bedb1d2eaf5ad4837901f46bfaecab736148910
haru <haruki1472@icloud.com>
2022-08-04 12:39:36 +0900

    trello風機能制作完了

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./components/Main";
 function App() {
   return (
     <div style={{ padding: "50px" }}>
+      <div className="side">aaa</div>
       <h1 style={{ padding: "20px" }}>Trello</h1>
       <Main />
     </div>
